1739617d34644ccc35108796cee2745deca08568
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-16 17:22:39 -0300

    config - RTK Query

---
+++ src/TodoApp.jsx
@@ -0,0 +1,15 @@
+
+
+export const TodoApp = () => {
+  return (
+    <>
+        <h1>Todos - RTK Query</h1>
+        <hr/>
+        <h4>Loading...</h4>
+
+        <pre>...</pre>
+
+        <button>Next Todo</button>
+    </>
+  )
+}
