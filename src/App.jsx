a7d719b4b94d0ba17f37bc7723517ba0b05d3350
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-16 15:16:14 -0300

    uses increment()

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useDispatch, useSelector } from "react-redux";
+
+import { increment } from "./store/slices/counter";
+
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { counter } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -18,13 +22,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => dispatch(increment())}>count is {counter}</button>
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
